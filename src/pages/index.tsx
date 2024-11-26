7c6c5793235a6239c45b1aaf56a963a205b9037b
goggle555 <goggle555@fives.tech>
2024-11-26 21:05:59 +0900

    feat: phaser-containerをトップページに実装する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import { HeadTemplate } from "@/components/head-template";
+import dynamic from "next/dynamic";
 import styles from "./index.module.css";
+
+const PhaserContainer = dynamic(import("@/components/phaser-container"), {
+  ssr: false,
+});
 
 export default function HomePage() {
   return (
     <>
       <HeadTemplate />
       <div className={`${styles.page}`}>
-        <main className={styles.main} />
-        <footer className={styles.footer} />
+        <main className={styles.main}>
+          <PhaserContainer />
+        </main>
       </div>
     </>
   );
